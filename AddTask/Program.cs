48f784294e474163b4b23bc81dbbf466b0ede9c2
Valera <vlaus0@gmail.com>
2023-06-27 21:03:15 +0300

    Добавьте файлы проекта.

---
+++ AddTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    class Program
+    {
+        static private int counter = 0;
+        static private SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+        static private async Task Function1Async()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                await semaphore.WaitAsync();
+                Console.Write("1 ");
+                Console.WriteLine("{0} з потоку {1}", ++counter, Thread.CurrentThread.GetHashCode());
+                semaphore.Release();
+            }
+        }
+
+        static private async Task Function2Async()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                await semaphore.WaitAsync();
+                Console.Write("2 ");
+                Console.WriteLine("{0} з потоку {1}", ++counter, Thread.CurrentThread.GetHashCode());
+                semaphore.Release();
+            }
+        }
+
+        static private async Task Function3Async()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                semaphore.Release();
+                await semaphore.WaitAsync();
+                Console.Write("3 ");
+                Console.WriteLine("{0} з потоку {1}", ++counter, Thread.CurrentThread.GetHashCode());
+            }
+        }
+
+        static async Task Main()
+        {
+            Thread[] threads = { new Thread(async () => await Function1Async()), new Thread(async () => await Function2Async()), new Thread(async () => await Function3Async()) };
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private bool isConnected;
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Ініціалізація таймера
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+
+            // Початковий стан підключення
+            isConnected = false;
+        }
+        private void OnConnectButtonClick(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "Підключено до бази даних";
+
+            // Імітація затримки для підключення
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            timer.Start();
+        }
+
+        private void OnDisconnectButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Затримка для імітації відключення від БД
+            textBox.Text = "Відключено від бази даних";
+            timer.Stop();
+        }
+
+        private void OnTimerTick(object state)
+        {
+            if (isConnected)
+            {
+                // Оновлення TextBox з повідомленням "Дані отримані"
+                UpdateTextBox("Дані отримані");
+            }
+        }
+
+        private void UpdateTextBox(string message)
+        {
+            // Оновлення TextBox з використанням диспетчера потоку для доступу до UI-елементів
+            Dispatcher.Invoke(() => textBox.Text = message);
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            textBox.Text = "Дані отримані. Час: " + DateTime.Now.ToString();
+        }
+    }
+}
